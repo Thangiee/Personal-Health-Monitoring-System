0179c2ae92f07eaebe2dcb224a0e70584b5bc794
Thang Le <thang.le@mavs.uta.edu>
2014-03-02 01:15:50 -0600

    add User class

---
+++ PHMS/src/main/java/com/cse3310/phms/model/User.java
@@ -0,0 +1,48 @@
+/*
+ * Copyright (c) 2014 Personal-Health-Monitoring-System
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * 	http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.cse3310.phms.model;
+
+import com.activeandroid.Model;
+import com.activeandroid.annotation.Column;
+import com.activeandroid.annotation.Table;
+
+@Table(name = "User")
+public class User extends Model{
+
+    @Column(name = "UserName")
+    private String username;
+
+    @Column(name = "Password")
+    private String password;
+
+    public User() {
+        super();
+    }
+
+    public User(String username, String password) {
+        this.username = username;
+        this.password = password;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+}
