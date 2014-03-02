e06f274dd437a453a3d52777c038495e6dedf904
Thang Le <thang.le@mavs.uta.edu>
2014-03-02 16:21:58 +0000

    Implement login method. Remove two un-needed helper util classes.

--- PHMS/src/main/java/com/cse3310/phms/model/LoginManager.java
+++ PHMS/src/main/java/com/cse3310/phms/model/LoginManager.java
@@ -23,25 +23,33 @@ public class LoginManager {
 
     private LoginManager() {}
 
-    public static boolean register(String userName, String password) {
+    public static boolean register(String userName, String password, PersonalInfo personalInfo) {
         if (!isUserNameExists(userName)) {
             String hashedPassword = BCrypt.hashpw(password, BCrypt.gensalt());
 
+            personalInfo.save();
             User newUser = new User(userName, hashedPassword);
+            newUser.setPersonalInfo(personalInfo);
             newUser.save();
             return true;
         } else {
             return false;
         }
-//            return newUser;
-//        } else {
-//            throw new IllegalArgumentException("Username is already taken.");
-//        }
     }
 
-//    public static boolean login(String userName, String password) {
-//
-//    }
+    public static boolean login(String userName, String password) {
+        User user = Database.getUserByUserName(userName);
+
+        // check if username is valid
+        if (user == null) { return false; }
+
+        // check if password is valid
+        return BCrypt.checkpw(password, user.getPassword());
+    }
+
+    public static void logOut() {
+        // todo: to be implemented
+    }
 
     private static boolean isUserNameExists(String userName) {
         User user = Database.getUserByUserName(userName);
