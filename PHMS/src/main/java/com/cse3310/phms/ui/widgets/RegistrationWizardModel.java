c05ac084c4088425889ac48b196a6701fe4537d0
Thang Le <thang.le@mavs.uta.edu>
2014-03-04 16:09:04 +0000

    Create working test wizard pager.

---
+++ PHMS/src/main/java/com/cse3310/phms/ui/widgets/RegistrationWizardModel.java
@@ -0,0 +1,35 @@
+/*
+ * Copyright (c) 2014 Personal-Health-Monitoring-System
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * 	http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.cse3310.phms.ui.widgets;
+
+import android.content.Context;
+import co.juliansuarez.libwizardpager.wizard.model.AbstractWizardModel;
+import co.juliansuarez.libwizardpager.wizard.model.CustomerInfoPage;
+import co.juliansuarez.libwizardpager.wizard.model.PageList;
+import co.juliansuarez.libwizardpager.wizard.model.TextPage;
+
+public class RegistrationWizardModel extends AbstractWizardModel {
+    public RegistrationWizardModel(Context context) {
+        super(context);
+    }
+
+    @Override
+    protected PageList onNewRootPageList() {
+        return new PageList(new CustomerInfoPage(this, "Test"),
+                new TextPage(this, "name"));
+    }
+}
