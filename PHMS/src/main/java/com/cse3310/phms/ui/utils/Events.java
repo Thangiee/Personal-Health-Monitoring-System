f9a6510129177650df639f5d10fd14071ca2b2e8
Mysteryy <zingercomputers@gmail.com>
2014-04-13 21:22:12 -0500

    Updated

--- PHMS/src/main/java/com/cse3310/phms/ui/utils/Events.java
+++ PHMS/src/main/java/com/cse3310/phms/ui/utils/Events.java
@@ -17,6 +17,7 @@
 package com.cse3310.phms.ui.utils;
 
 import com.cse3310.phms.ui.cards.FoodCard;
+import com.cse3310.phms.ui.cards.MedicationCard;
 import it.gmariotti.cardslib.library.internal.Card;
 
 import java.util.Collection;
@@ -59,6 +60,22 @@ public class Events {
 
         public AddFoodCardEvent(FoodCard foodCard) {
             this.foodCard = foodCard;
+        }
+    }
+
+    public static class AddMedicationCardEvent {
+        public MedicationCard medicationCard;
+
+        public AddMedicationCardEvent(MedicationCard medicationCard) {
+            this.medicationCard = medicationCard;
+        }
+    }
+
+    public static class RemoveMedicationCardEvent {
+        public MedicationCard medicationCard;
+
+        public RemoveMedicationCardEvent(MedicationCard medicationCard) {
+            this.medicationCard = medicationCard;
         }
     }
 
