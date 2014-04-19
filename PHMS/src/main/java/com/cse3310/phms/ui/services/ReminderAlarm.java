377725784f78f830f7d8cd99c3b3e6320cfd9fd5
Thang Le <thang.le@mavs.uta.edu>
2014-04-19 19:35:48 +0000

    add alarm reminder implementation

---
+++ PHMS/src/main/java/com/cse3310/phms/ui/services/ReminderAlarm.java
@@ -0,0 +1,87 @@
+/*
+ * Copyright (c) 2014 Personal-Health-Monitoring-System
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * 	http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.cse3310.phms.ui.services;
+
+import android.app.AlarmManager;
+import android.app.Notification;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.media.RingtoneManager;
+import android.net.Uri;
+import android.os.Bundle;
+import com.cse3310.phms.R;
+import com.cse3310.phms.model.Remindable;
+
+public class ReminderAlarm extends BroadcastReceiver {
+    public static final int ID = 1;
+    private final String REMINDER_BUNDLE = "MyReminderBundle";
+
+    public ReminderAlarm() {
+    }
+
+    public ReminderAlarm(Context context, Remindable remindable, int icon) {
+        final int id = (int) System.currentTimeMillis();
+
+        Bundle bundle = new Bundle();
+        bundle.putString("message", remindable.reminderMessage());
+        bundle.putInt("icon", icon);
+
+        Intent intent = new Intent(context, ReminderAlarm.class);
+        intent.putExtra(REMINDER_BUNDLE, bundle);
+        PendingIntent pendingIntent = PendingIntent.getBroadcast(context, id, intent, PendingIntent.FLAG_ONE_SHOT);
+
+        AlarmManager alarmManager = (AlarmManager) context.getSystemService(Context.ALARM_SERVICE);
+        alarmManager.set(AlarmManager.RTC_WAKEUP, remindable.reminderTime(), pendingIntent);    // set the alarm
+    }
+
+
+    @Override
+    public void onReceive(Context context, Intent intent) {
+        //Create an Intent for the BroadcastReceiver
+        Intent buttonIntent = new Intent(context, AlarmButtonReceiver.class);
+        buttonIntent.putExtra("id", ID);
+
+        //Create the PendingIntent
+        PendingIntent btnPendingIntent = PendingIntent.getBroadcast(context, 0, buttonIntent, 0);
+
+        // get sound for notification
+        Uri alarmSound = RingtoneManager.getDefaultUri(RingtoneManager.TYPE_ALARM);
+
+        Bundle bundle = intent.getBundleExtra(REMINDER_BUNDLE);
+        String message = bundle.getString("message");
+        int icon = bundle.getInt("icon");
+
+        Notification.Builder builder = new Notification.Builder(context)
+                .setSmallIcon(icon)
+                .setSound(alarmSound)
+                .setContentTitle("Reminder!")
+                .addAction(R.drawable.ic_check_light, "Dismiss", btnPendingIntent); //Pass this PendingIntent to addAction method of Intent Builder//
+
+        Notification notification = new Notification.BigTextStyle(builder)
+                .bigText(message)
+                .build();
+
+        notification.defaults|= Notification.DEFAULT_VIBRATE; // enable vibration
+        notification.flags |= Notification.FLAG_INSISTENT;  // repeat sound?
+
+        NotificationManager nm = (NotificationManager) context.getSystemService(Context.NOTIFICATION_SERVICE);
+        nm.notify(ID, notification);    // fire the notification
+    }
+}
